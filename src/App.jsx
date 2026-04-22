9510badcdfc7abfe6922a86a74fd95cff0c43028
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-22 13:42:27 +0200

    feat: update SubjectCard component to accept props and add prop types; enhance App structure and remove unused Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import SubjectCard from "./SubjectCard/SubjectCard.jsx";
 function App() {
   return(
     <>
-      <SubjectCard/>
+      <SubjectCard subjectName="Computer Science" imageSrc="https://avatars.githubusercontent.com/u/62205605?v=4"/>
+      <SubjectCard subjectName="Data Structures"/>
     </>
   );
 }
--- src/SubjectCard/SubjectCard.jsx
+++ src/SubjectCard/SubjectCard.jsx
@@ -1,12 +1,18 @@
 import styles from "./SubjectCard.module.css";
+import PropTypes from 'prop-types';
 
-function SubjectCard() {
+function SubjectCard(props) {
     return(
         <div className={styles.subjectCard}>
-            <h2 className={styles.subjectName}>Computer Science</h2>
-            <img alt="profile picture" src="https://avatars.githubusercontent.com/u/62205605?v=4" className={styles.subjectImage}/>
+            <h2 className={styles.subjectName}>{props.subjectName}</h2>
+            <img alt="profile picture" src={props.imageSrc} className={styles.subjectImage}/>
         </div>
     );
+}
+
+SubjectCard.propTypes = {
+    subjectName: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string
 }
 
 export default SubjectCard
