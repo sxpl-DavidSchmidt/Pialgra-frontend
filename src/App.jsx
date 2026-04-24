0bb4832794c55eb16269f20fc800ea7f9c72765c
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-24 22:30:07 +0200

    refactor: update workedHours calculation to use daysInMonth for improved accuracy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
+import { useMemo } from "react";
 import SubjectCard from "./SubjectCard/SubjectCard.jsx";
 import TimeTable from "./TimeTable/TimeTable.jsx";
 
 function App() {
-  const workedHours = Array.from({ length: 31 }, () => Math.floor(Math.random() * 8));
+  const year = 2026;
+  const month = 0;
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-  return(
+  const workedHours = Array.from({ length: daysInMonth }, () =>
+    Math.floor(Math.random() * 8)
+  );
+
+  return (
     <>
-      <SubjectCard subjectName="Computer Science" imageSrc="https://avatars.githubusercontent.com/u/62205605?v=4"/>
-      <SubjectCard subjectName="Data Structures"/>
-      <TimeTable year={2026} month={0} workedHours={workedHours} cellSize={15} subjectName="Computer Science" color="#000000"/>
+      <SubjectCard
+        subjectName="Computer Science"
+        imageSrc="https://avatars.githubusercontent.com/u/62205605?v=4"
+      />
+
+      <SubjectCard
+        subjectName="Data Structures"
+      />
+      
+      <TimeTable
+        year={year}
+        month={month}
+        workedHours={workedHours}
+        cellSize={15}
+        subjectName="Computer Science"
+        color="#000000"
+      />
     </>
   );
 }
 
-export default App
+export default App;
