4fe1f50204efd518d287a5b15a5dec5fbad67cb7
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-05-26 17:30:46 +0200

    feat: implement basic Timer function with start and reset

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+
 import styles from "./Timer.module.css";
 
 import PlayIcon from "../../assets/icons/play.svg?react"
@@ -5,13 +7,51 @@ import ResetIcon from "../../assets/icons/reset.svg?react"
 import ArrowIcon from "../../assets/icons/arrow_down.svg?react"
 
 export default function Timer() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    }
+
+    return () => {
+      clearInterval(intervalIdRef.current);
+    }
+  }, [isRunning]);
+
+  function start() {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+  }
+
+  function stop() {
+    setIsRunning(false);
+  }
+
+  function reset() {
+    setElapsedTime(0);
+    setIsRunning(false);
+  }
+
+  function formatTime() {
+    let minutes = Math.floor(elapsedTime / (1000 * 60));
+    let seconds = Math.floor(elapsedTime / (1000) % 60);
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.clockContainer}>
         <div className={styles.clockText}>
-          <h1>45:00</h1>
+          <h1>{formatTime()}</h1>
         </div>
-        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" style={{ rotate: "90deg"}}>
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" style={{ rotate: "90deg" }}>
           <path className={styles.clockOuter}
             pathLength={1}
             fill="none"
@@ -40,11 +80,11 @@ export default function Timer() {
           <option>Psychology</option>
         </select>
 
-        <button className={styles.startButton}>
+        <button onClick={start} className={styles.startButton}>
           <PlayIcon className={styles.startButtonIcon} />
           <p>Start</p>
         </button>
-        <button className={styles.resetButton}>
+        <button onClick={reset} className={styles.resetButton}>
           <ResetIcon className={styles.resetButtonIcon} />
           <p>Reset</p>
         </button>
@@ -52,19 +92,19 @@ export default function Timer() {
         <div className={styles.timeSelectWrap} style={{ gridArea: "workTimer" }}>
           <p>Work Duration</p>
           <div className={styles.timeSelect}>
-            <button><ArrowIcon style={{ rotate: "90deg" }}/></button>
+            <button><ArrowIcon style={{ rotate: "90deg" }} /></button>
             <p>{45}m</p>
-            <button><ArrowIcon style={{ rotate: "-90deg" }}/></button>
+            <button><ArrowIcon style={{ rotate: "-90deg" }} /></button>
           </div>
         </div>
 
         <div className={styles.timeSelectWrap} style={{ gridArea: "breakTimer" }}>
           <p>Break Duration</p>
           <div className={styles.timeSelect}>
-            <button><ArrowIcon style={{ rotate: "90deg" }}/></button>
+            <button><ArrowIcon style={{ rotate: "90deg" }} /></button>
             <p>{15}m</p>
-            <button><ArrowIcon style={{ rotate: "-90deg" }}/></button>
-            
+            <button><ArrowIcon style={{ rotate: "-90deg" }} /></button>
+
           </div>
         </div>
 
